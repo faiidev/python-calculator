e70994f967198a07c784757d1d56b1e8a6681f83
Mhd Fairuz Arkananta <fairuzarka76@gmail.com>
2026-07-06 09:36:30 +0700

    Add files via upload

---
+++ Calculator V1.py
@@ -0,0 +1,23 @@
+print("=====================")
+print("CALCULATOR V1")
+print("=====================")
+
+angka_pertama = int(input("Masukkan Angka Pertama : "))
+angka_kedua = int(input("Masukkan Angka Kedua   : "))
+
+print("=====================")
+
+hasil_penjumlahan = angka_pertama + angka_kedua
+hasil_pengurangan = angka_pertama - angka_kedua
+hasil_perkalian = angka_pertama * angka_kedua
+hasil_pembagian = angka_pertama / angka_kedua
+hasil_sisa_bagi = angka_pertama % angka_kedua
+hasil_pangkat = angka_pertama ** angka_kedua
+
+print("Hasil Penjumlahan : ", hasil_penjumlahan)
+print("Hasil Pengurangan : ", hasil_pengurangan)
+print("Hasil Perkalian   : ", hasil_perkalian)
+print("Hasil Pembagian   : ", hasil_pembagian)
+print("Hasil Sisa Bagi   : ", hasil_sisa_bagi)
+print("Hasil Pangkat     : ", hasil_pangkat)   
+print("=====================")
